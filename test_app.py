b10be18004c568645bb14114394c936f13937b2b
Benjamin Metz <b@banno.com>
2023-07-02 10:43:07 -0500

    adding inital test file for api

---
+++ test_app.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_welcome():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to forge API!"}
