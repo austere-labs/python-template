234afcf1a3fc0d5020db8f05de552436bfa86562
Benjamin Metz <b@banno.com>
2024-05-03 18:27:53 -0500

    updating main to remove logging stuff

--- main.py
+++ main.py
@@ -1,33 +1,20 @@
 from fastapi import FastAPI
 from router import router
-import logging
-import sys
-from pythonjsonlogger import jsonlogger
 import uvicorn
 from config import Config
+from log_config import LoggingConfig
+import logging
 
 app = FastAPI()
 app.include_router(router)
 
+log_config = LoggingConfig(log_level=logging.INFO)
+logger = log_config.get_logger()
+
 
 def main():
-    # Setup logging
-    logger = logging.getLogger("indicators-service")
-    logger.setLevel(logging.DEBUG)
-    # create handler for logging to stdout
-    stdoutHandler = logging.StreamHandler(stream=sys.stdout)
-
-    stdoutHandler.setLevel(logging.DEBUG)
-    fmt = jsonlogger.JsonFormatter(
-        "%(name)s %(asctime)s %(levelname)s %(filename)s %(lineno)s %(process)d %(message)s",
-        rename_fields={"levelname": "severity", "asctime": "timestamp"},
-    )
-    stdoutHandler.setFormatter(fmt)
-    logger.addHandler(stdoutHandler)
-    logger.info("Starting indicators service...")
-
     config = Config()
-    print(config.port)
+    logger.info(f"Starting indicators service on port {config.port}...")
     uvicorn.run("main:app", host="0.0.0.0", port=int(config.port))
 
 
