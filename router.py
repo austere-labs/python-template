f8c1bce62e02f326e298d21f442d180295c81691
Benjamin Metz <b@banno.com>
2024-05-03 18:28:28 -0500

    updating router to include logging

--- router.py
+++ router.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from log_config import LoggingConfig
+import logging
 
-router = APIRouter()
+routes = APIRouter()
+log_config = LoggingConfig(log_level=logging.INFO)
 
 
-@router.get("/")
-async def welcome():
+@routes.get("/")
+async def welcome(logger: logging.Logger = Depends(log_config.get_logger)):
+    logger.info("Welcome endpoint hit!")
     return {"message": "Welcome to template API service!"}
