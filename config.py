0d9769a4c6de0037678cb3c5254ede7396c13746
Benjamin Metz <b@banno.com>
2024-05-04 15:33:46 -0500

    updating .env and config to reflect new env vars

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config:
 
     def __init__(self) -> None:
         self.port = os.getenv("PORT")
+        self.gcp_project_id = os.getenv("GCP_PROJECT_ID")
+        self.test_secret_key = os.getenv("TEST_SECRET_KEY")
--- test_config.py
+++ test_config.py
@@ -4,3 +4,8 @@ from config import Config
 def test_config_port():
     config = Config()
     assert config.port == "8081"
+    assert config.gcp_project_id == "482777410016"
+    assert (
+        config.test_secret_key
+        == "projects/482777410016/secrets/TestSecretForTests/versions/1"
+    )
